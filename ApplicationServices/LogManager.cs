b9939fc1097dbf998dbd4d315c2691c8487f4a09
hircjusz <darek.uchman@gmail.com>
2015-11-30 22:47:20 +0100

    LogManager

---
+++ ApplicationServices/LogManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StateMachine;
+
+namespace ApplicationServices
+{
+    public interface ILogManager
+    {
+        void Log(object sender, StateMachineEventArgs args);
+    }
+
+    public class LogManager : ILogManager
+    {
+         public void Log(object sender, StateMachineEventArgs args)
+         {
+             Debug.Print(args.EventName);
+         }
+
+
+    }
+}
