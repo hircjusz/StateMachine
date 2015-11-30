6031fe8705e29d3c5e8d57438a37d2293d89c72a
hircjusz <darek.uchman@gmail.com>
2015-11-30 19:50:05 +0100

    event manager

---
+++ ApplicationServices/EventManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StateMachine;
+
+namespace ApplicationServices
+{
+    public class EventManager
+    {
+
+        private Dictionary<string, object> EventList;
+
+        public EventHandler<StateMachineEventArgs> EventManagerEvent;
+
+        private static readonly Lazy<EventManager> _eventManager= new Lazy<EventManager>(()=>new EventManager());
+
+        public static EventManager Instance
+        {
+            get { return _eventManager.Value; }
+
+        }
+
+        private EventManager()
+        {
+            EventList= new Dictionary<string, object>();
+        }
+
+
+
+    }
+}
